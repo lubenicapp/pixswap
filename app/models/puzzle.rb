8ef371493c0bf792df9e0a22908855cec2e85315
Leon Lagadec <leon.lagadec@perfect-memory.com>
2022-05-31 12:41:08 +0200

    model for puzzle

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,3 +1,35 @@
+class ValidateFeasibility < ActiveModel::Validator
+  def validate(record)
+    unless record.start.split('').sort == record.end.split('').sort
+      record.errors.add :end, "end and start position should have the same number of squares"
+    end
+  end
+end
+
+class ValidateStringValidity < ActiveModel::Validator
+  def validate(record)
+    unless record.start.split('').uniq.sort == ["0", "1"]
+      record.errors.add :start, "string must contains 0 and 1 only"
+    end
+  end
+end
+
+
+
 class Puzzle < ApplicationRecord
+
+  include ActiveModel::Validations
+  validates_with ValidateFeasibility
+  validates_with ValidateStringValidity
+
+  validates :start,
+            presence: true,
+            length: { is: 25 }
+  validates :end,
+            presence: true,
+            length: { is: 25 }
+  validates :status,
+            presence: true,
+            inclusion: { in: %w(solved, unsolved) }
 
 end
--- db/migrate/20220531100918_create_puzzles.rb
+++ db/migrate/20220531100918_create_puzzles.rb
@@ -1,9 +1,13 @@
-*class CreatePuzzles < ActiveRecord::Migration[7.0]
+class CreatePuzzles < ActiveRecord::Migration[7.0]
   def change
     create_table :puzzles do |t|
       t.integer :minimum_moves
+      t.integer :best_score
+
       t.string :start
       t.string :end
+      t.string :current
+
       t.string :status
 
       t.timestamps
