190be9d484dd3aa67030f0a8648563641d8efbe3
Leon Lagadec <leon.lagadec@perfect-memory.com>
2022-05-31 17:00:45 +0200

    shift line left and right

--- app/services/matrix_manipulator.rb
+++ app/services/matrix_manipulator.rb
@@ -10,7 +10,36 @@ module MatrixManipulator
 
   def line(line_index, arr = self.current)
     r = ''
-    MATRIX_SIZE.times { |t| r += arr[5 * l + t] }
+    MATRIX_SIZE.times { |t| r += arr[5 * line_index + t] }
     r
   end
+
+  def shift_line_left(line_index)
+    r = ''
+    MATRIX_SIZE.times do |i|
+      if i == line_index
+        transform = line(line_index).split('')
+        transform = transform.push(transform.shift).join('')
+        r += transform
+      else
+        r += line(i)
+      end
+    end
+    self.current = r
+  end
+
+  def shift_line_right(line_index)
+    r = ''
+    MATRIX_SIZE.times do |i|
+      if i == line_index
+        transform = line(line_index).split('')
+        transform = transform.unshift(transform.pop).join('')
+        r += transform
+      else
+        r += line(i)
+      end
+    end
+    self.current = r
+  end
+
 end
--- spec/app/models/puzzle_spec.rb
+++ spec/app/models/puzzle_spec.rb
@@ -44,4 +44,43 @@ describe Puzzle, type: :model do
       it { is_expected.to be false }
     end
   end
+
+  describe '#line' do
+    subject(:line) { puzzle.line(index) }
+    let(:attributes) { {current: "1111100000111110000011111"} }
+    let(:index) { 0 }
+    context 'with a simple matrix' do
+        it {is_expected.to eq('11111')}
+      end
+  end
+
+  describe '#column' do
+    subject (:column) { puzzle.column(index) }
+    let(:attributes) { {current: "1000010000100001000010000"} }
+    let(:index) { 0 }
+    context 'with a simple matrix' do
+      it {is_expected.to eq('11111')}
+    end
+  end
+
+  describe '#shift_line_left' do
+    subject(:shift_line_left) { puzzle.shift_line_left(args) }
+    let(:attributes) { {current: "1000000000000000000000000"} }
+    let(:args) { 0 }
+    it 'offsets the given line by one' do
+      shift_line_left
+      expect(puzzle.current).to eq("0000100000000000000000000")
+    end
+  end
+
+  describe '#shift_line_right' do
+    subject(:shift_line_right) { puzzle.shift_line_right(args) }
+    let(:attributes) { {current: "1000000000000000000000000"} }
+    let(:args) { 0 }
+    it 'offsets the given line by one' do
+      shift_line_right
+      expect(puzzle.current).to eq("0100000000000000000000000")
+    end
+  end
+
 end
