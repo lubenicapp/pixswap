1f8f6ecf939f674ff6b8866922a03bbe64e751f4
Leon Lagadec <leon.lagadec@perfect-memory.com>
2022-05-31 13:02:10 +0200

    validation : position are differents

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,11 +1,18 @@
+#
+# Validate if the end position is reachable from start
+#  ie : same count of tiles
+#
 class ValidateFeasibility < ActiveModel::Validator
   def validate(record)
     unless record.start.nil? || record.end.nil? || record.start.split('').sort == record.end.split('').sort
-      record.errors.add :end, "end and start position should have the same number of squares"
+      record.errors.add :end, "end and start position should have the same tiles"
     end
   end
 end
 
+#
+#  Validates if start position contains 0 and 1 and only this
+#
 class ValidateStringValidity < ActiveModel::Validator
   def validate(record)
     unless record.start.nil? || record.end.nil? || record.start.split('').uniq.sort == ["0", "1"]
@@ -14,12 +21,19 @@ class ValidateStringValidity < ActiveModel::Validator
   end
 end
 
-
+#
+# Validates if start and end position are different
+#
+class ValidatePositionsAreDifferent < ActiveModel::Validator
+  def validate(record)
+    unless record.start == record.end
+      record.errors.add :start, "start and end positions must be different"
+    end
+  end
+end
 
 class Puzzle < ApplicationRecord
-
   include ActiveModel::Validations
-
 
   validates :start,
             presence: true,
@@ -27,11 +41,8 @@ class Puzzle < ApplicationRecord
   validates :end,
             presence: true,
             length: { is: 25, allow_nil: false }
-  validates :status,
-            presence: true,
-            inclusion: { in: %w(solved, unsolved) }
 
   validates_with ValidateFeasibility
   validates_with ValidateStringValidity
-
+  validates_with ValidatePositionsAreDifferent
 end
