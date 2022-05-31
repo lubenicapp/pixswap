d6198892a1213dea23ec271fe9735f662fa8c564
Leon Lagadec <leon.lagadec@perfect-memory.com>
2022-05-31 12:51:24 +0200

    validation for puzzle

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,6 +1,6 @@
 class ValidateFeasibility < ActiveModel::Validator
   def validate(record)
-    unless record.start.split('').sort == record.end.split('').sort
+    unless record.start.nil? || record.end.nil? || record.start.split('').sort == record.end.split('').sort
       record.errors.add :end, "end and start position should have the same number of squares"
     end
   end
@@ -8,7 +8,7 @@ end
 
 class ValidateStringValidity < ActiveModel::Validator
   def validate(record)
-    unless record.start.split('').uniq.sort == ["0", "1"]
+    unless record.start.nil? || record.end.nil? || record.start.split('').uniq.sort == ["0", "1"]
       record.errors.add :start, "string must contains 0 and 1 only"
     end
   end
@@ -19,17 +19,19 @@ end
 class Puzzle < ApplicationRecord
 
   include ActiveModel::Validations
-  validates_with ValidateFeasibility
-  validates_with ValidateStringValidity
+
 
   validates :start,
             presence: true,
-            length: { is: 25 }
+            length: { is: 25, allow_nil: false }
   validates :end,
             presence: true,
-            length: { is: 25 }
+            length: { is: 25, allow_nil: false }
   validates :status,
             presence: true,
             inclusion: { in: %w(solved, unsolved) }
+
+  validates_with ValidateFeasibility
+  validates_with ValidateStringValidity
 
 end
