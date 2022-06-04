fab5602259df96dbfa62ea1974b7472000c686a5
Leon Lagadec <leon.lagadec@perfect-memory.com>
2022-06-04 20:43:35 +0200

    refacto routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,16 @@
 Rails.application.routes.draw do
   root 'puzzles#index'
 
-  resources :puzzles, only: [:index, :show]
-  post '/puzzles/:id/move', to: 'puzzles#move'
+  resources :puzzles, only: [:index, :show] do
+    member do
+      post '/move', to: "puzzles#move"
+      post '/reset', to: "puzzles#reset_one"
+    end
+    collection do
+      post "/reset", to: "puzzles#reset_all"
+    end
+  end
 
-  #
-  # show next puzzle to solve
-  #
   get '/challenge', to: 'puzzles#challenge'
 
-  #
-  # reset all progression
-  #
-  post 'puzzles/reset', to: 'puzzles#reset_all'
-  post 'puzzles/:id/reset', to: 'puzzles#reset_one'
 end
