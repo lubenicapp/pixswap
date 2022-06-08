768e2abd10c502eabee3135feb825e16d61244f7
Leon Lagadec <leon.lagadec@perfect-memory.com>
2022-06-08 15:58:12 +0200

    Fixed CI

--- src/api/spec/rails_helper.rb
+++ src/api/spec/rails_helper.rb
@@ -33,6 +33,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+DatabaseCleaner.allow_remote_database_url = true
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
